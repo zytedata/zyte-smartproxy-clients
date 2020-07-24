f600720168af2aeda6a963e52a0e96ab907acd08
krant <aleksey.vasilenko@gmail.com>
2020-07-24 12:51:21 +0300

    Better var name

--- client.R
+++ client.R
@@ -1,10 +1,10 @@
 library(httr)
 
-hostport <- strsplit(Sys.getenv("PROXY"), ":")[[1]]
+proxy <- strsplit(Sys.getenv("PROXY"), ":")[[1]]
 
 r <- GET(Sys.getenv("URL"), config(cainfo = "crawlera-ca.crt"), use_proxy(
-    url = hostport[1],
-    port = strtoi(hostport[2]),
+    url = proxy[1],
+    port = strtoi(proxy[2]),
     username = Sys.getenv("KEY"),
     password = ""
   ))
