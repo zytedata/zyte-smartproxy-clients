8ccb07cbc836f3c71aaaf42835a664e6eb1c501e
krant <aleksey.vasilenko@gmail.com>
2020-07-28 18:46:00 +0300

    R: formatting

--- client.R
+++ client.R
@@ -1,16 +1,20 @@
 library(httr)
 
-proxy <- strsplit(Sys.getenv("PROXY"), ":")[[1]]
+proxy <- strsplit(Sys.getenv('PROXY'), ':')[[1]]
 
-r <- GET(Sys.getenv("URL"), config(cainfo = "crawlera-ca.crt"), use_proxy(
+r <- GET(
+  Sys.getenv('URL'),
+  config(cainfo = 'crawlera-ca.crt'),
+  use_proxy(
     url = proxy[1],
     port = strtoi(proxy[2]),
-    username = Sys.getenv("KEY"),
-    password = ""
-  ))
+    username = Sys.getenv('KEY'),
+    password = ''
+  )
+)
 
 print(r)
 
 if (r$status_code != 200) {
-  quit(status=1)
+  quit(status = 1)
 }
