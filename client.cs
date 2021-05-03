625f7591ecd83bfcd1121bc4875ae3a79b97dd35
Aleksey Vasilenko <aleksey.vasilenko@gmail.com>
2021-05-03 09:55:16 +0300

    Add Julia and .NET 5.0 (#2)

---
+++ client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace zyte
+{
+    class client
+    {
+        static async Task<int> Main()
+        {
+            var http = new HttpClient(
+                new SocketsHttpHandler {
+                    Proxy = new WebProxy {
+                        Address = new Uri("http://" + System.Environment.GetEnvironmentVariable("PROXY")),
+                        Credentials = new NetworkCredential(System.Environment.GetEnvironmentVariable("KEY"), "")
+                    }
+            });
+
+            var response = await http.GetAsync(System.Environment.GetEnvironmentVariable("URL"));
+            Console.Write(await response.Content.ReadAsStringAsync());
+
+            return response.IsSuccessStatusCode ? 0 : -1;
+        }
+    }
+}
