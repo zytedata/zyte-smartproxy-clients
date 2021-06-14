3627fc7897da4628196c162fa876b7ed31184416
krant <aleksey.vasilenko@gmail.com>
2021-06-14 16:29:37 +0300

    More idiomatic code, deps update

--- client.js
+++ client.js
@@ -1,24 +1,13 @@
 var request = require('request');
 var fs = require('fs');
 
-var options = {
-    url: process.env.URL,
+request({
+    uri: process.env.URL,
     ca: fs.readFileSync("crawlera-ca.crt"),
-};
-
-var new_req = request.defaults({
-    'proxy': 'http://' + process.env.KEY + ':@' + process.env.PROXY
+    proxy: 'http://' + process.env.KEY + ':@' + process.env.PROXY
+}, function callback(error, response, body) {
+    console.log(error, body);
+    if (error || response.statusCode != 200) {
+        process.exit(1);
+    }
 });
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(response.headers);
-        console.log(body);
-    }
-    else{
-        console.log(error, response, body);
-        process.exit(1)
-    }
-}
-
-new_req(options, callback);
