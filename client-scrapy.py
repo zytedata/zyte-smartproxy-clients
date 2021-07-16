3f854e1fca12aec710369471fb0bd439dba00607
Adrián Chaves <adrian@chaves.io>
2021-07-16 16:14:46 +0200

    Blind attempt 2

--- client-scrapy.py
+++ client-scrapy.py
@@ -8,6 +8,9 @@ class ClientSpider(scrapy.Spider):
     zyte_smartproxy_url = 'http://' + os.environ.get('PROXY')
 
     def start_requests(self):
+        self.logger.debug(f'Key: {self.zyte_smartproxy_apikey!r}')
+        self.logger.debug(f'Proxy URL: {self.zyte_smartproxy_url!r}')
+        self.logger.debug(f'URL: {os.environ.get('URL')!r}')
         yield scrapy.Request(url=os.environ.get('URL'))
 
     def parse(self, response):
